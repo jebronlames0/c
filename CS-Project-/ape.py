5e67d4e14b55a11709f8a909eefccc38790734c2
jebronlames0 <94163926+jebronlames0@users.noreply.github.com>
2021-11-13 10:39:37 +0530

    .

--- CS-Project-/ape.py
+++ CS-Project-/ape.py
@@ -6,7 +6,7 @@ import imutils
 import cv2
 from nembir_pilate_localijation import main as number_plate_localizer
 from save_in_csv import *
-
+from PIL import Image
 from imaje_resije import conTO28x28, image_resize_sklearn
 
 
@@ -116,60 +116,73 @@ def flask_app():
 
 def stream_lit_app():
 #STREAMLIT_APP
-    import cv2
-    from nembir_pilate_localijation import main as number_plate_localizer
-    import streamlit as st  #Web App
-    from PIL import Image #Image Processing
-    import numpy as np #Image Processing 
-    import os
+    try:
+        import cv2
+        from nembir_pilate_localijation import main as number_plate_localizer
+        import streamlit as st  #Web App
+        from PIL import Image #Image Processing
+        import numpy as np #Image Processing 
+        import os
 #title
-    st.title("NUMBER PLATE RECOGNITION")
+        st.title("NUMBER PLATE RECOGNITION")
 #subtitle
-    st.markdown("")
+        st.markdown("")
 
 #image uploader
-    image = st.file_uploader(label = "Upload the image of the car here",type=['png','jpg','jpeg'])
-    if image is not None:
-        #CHECKING IF IMAGE EXISTS OR NOT 
+        image = st.sidebar.file_uploader(label = "Upload the image of the car here",type=['png','jpg','jpeg'])
+        if image is not None:
+            #CHECKING IF IMAGE EXISTS OR NOT 
 
-        input_image = Image.open(image) #read image
-        file_details = image.name,image.type
-        with open(os.path.join("tempdir" ,"temp.png"),"wb") as f:
-            f.write(image.getbuffer())
-        #saving numberplate in a directory called tempdir
-
-        st.image(input_image) 
+            input_image = Image.open(image) #read image
+            file_details = image.name,image.type
+            with open(os.path.join("tempdir" ,"temp.png"),"wb") as f:
+                f.write(image.getbuffer())
+            #saving numberplate in a directory called tempdir
+        
+            st.sidebar.image(input_image) 
         #display image
-        st.write(file_details[0])
+            st.sidebar.write(file_details[0])
+            
         #displaying name of file user uploaded
         
-        st.success("Image successfully uploaded!")
-        st.balloons()
-        if st.button("Click here to read the numer plate!"):
-            number_plate_localizer("tempdir/temp.png")
-            format='.png'
-            myDir = "plates"
-            def createFileList(myDir, format='.png'):
-                fileList = []
-                for root, dirs, files in os.walk(myDir, topdown=False):
-                   for name in files:
-                      print(name)
-                      if name.endswith(format):
-                         fullName = os.path.join(root, name)
-                         fileList.append(fullName)
-                return fileList
-            for image in createFileList(myDir):
-               pred = predict_image(image)
-               if len(pred) > 3:
-                 st.write(pred)
-                 save_in_csv()
-               else:
-                 continue
-                 
-    else:
-         st.write("Upload an Image")
+            st.sidebar.success("Image successfully uploaded!")
+            st.balloons()
+            if st.sidebar.button("Click here to read the numer plate!"):
+                number_plate_localizer("tempdir/temp.png")
+                format='.png'
+                myDir = "plates"
+                def createFileList(myDir, format='.png'):
+                    fileList = []
+                    for root, dirs, files in os.walk(myDir, topdown=False):
+                       for name in files:
+                         # print(name)
+                          if name.endswith(format):
+                             fullName = os.path.join(root, name)
+                             fileList.append(fullName)
+                    return fileList
+                for image in createFileList(myDir):
+                 try:
+                   pred = predict_image(image)
+                   if len(pred) > 3:
+                     img=Image.open(image)
+                     st.image(img , caption="Localized Numberplate")
+                     
+                     st.write("prediction =",  pred)
+                     save_in_csv()
+                 except:
+                     continue
+                   
 
-    st.caption('''BY Sathish 
+        else:
+         st.sidebar.write("Upload an Image")
+
+       
+    except:
+        st.write("Can't Read the Numberplate")
+       
+                     
+
+        st.caption('''BY Sathish 
                   and Amogh''')     
 
 #*********************************************************************************************************************************************************************************
